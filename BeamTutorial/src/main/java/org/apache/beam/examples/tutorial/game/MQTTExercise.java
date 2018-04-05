573ef9eb1d7249a59857b1872e8bd4382c24a8f0
Nitin Kumar <nitinkr0411@gmail.com>
2018-04-05 20:02:47 +0530

    Added MQTT write and word count

--- BeamTutorial/src/main/java/org/apache/beam/examples/tutorial/game/MQTTExercise.java
+++ BeamTutorial/src/main/java/org/apache/beam/examples/tutorial/game/MQTTExercise.java
@@ -22,39 +22,68 @@ import org.apache.beam.examples.tutorial.game.utils.ExerciseOptions;
 import org.apache.beam.examples.tutorial.game.utils.Input;
 import org.apache.beam.examples.tutorial.game.utils.Output;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.DefaultFilenamePolicy;
+import org.apache.beam.sdk.io.FileBasedSink;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.io.mqtt.MqttIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Duration;
+
+import javax.annotation.Nullable;
 
 public class MQTTExercise {
+    //org.apache.beam.examples.tutorial.game.MQTTExercise
+    public static class ExtractWordsFn extends DoFn<byte[], String> {
 
-  /**
-   * A transform to extract key/score information from SensorData, and sum
-   * the scores. The constructor arg determines whether 'team' or 'user' info is
-   * extracted.
-   */
-  public static class ExtractAndSumScore
-      extends PTransform<PCollection<SensorData>, PCollection<KV<String, Integer>>> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            // Split the line into words.
+            try {
+                String json = new String(c.element(), "UTF-8");
+                String[] words = json.split("[^a-zA-Z']+");
 
-    private final KeyField field;
+                // Output each word encountered into the output PCollection.
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        c.output(word);
+                    }
+                }
+            } catch (Exception e) {
 
-    ExtractAndSumScore(KeyField field) {
-      this.field = field;
+            }
+        }
     }
 
-    @Override
-    public PCollection<KV<String, Integer>> expand(PCollection<SensorData> gameInfo) {
-      return gameInfo
-        .apply(new ChangeMe<>() /* TODO: YOUR CODE GOES HERE */)
-        .apply(new ChangeMe<>() /* TODO: YOUR CODE GOES HERE */);
-    }
-  }
+    public static class CountWords extends PTransform<PCollection<byte[]>,
+            PCollection<KV<String, Long>>> {
+        @Override
+        public PCollection<KV<String, Long>> expand(PCollection<byte[]> lines) {
 
-  /**
-   * Run a batch pipeline.
-   */
+            // Convert lines of text into individual words.
+            PCollection<String> words = lines.apply(
+                    ParDo.of(new ExtractWordsFn()));
+
+            // Count the number of times each word occurs.
+            PCollection<KV<String, Long>> wordCounts =
+                    words.apply(Count.perElement());
+
+            return wordCounts;
+        }
+    }
+
+    /** A SimpleFunction that converts a Word and Count into a printable string. */
+    public static class FormatAsTextFn extends SimpleFunction<KV<String, Long>, byte[]> {
+        @Override
+        public byte[] apply(KV<String, Long> input) {
+            return (input.getKey() + ": " + input.getValue()).getBytes();
+        }
+    }
   public static void main(String[] args) throws Exception {
     // Begin constructing a pipeline configured by commandline flags.
     ExerciseOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(ExerciseOptions.class);
@@ -65,7 +94,14 @@ public class MQTTExercise {
         .apply(MqttIO.read()
                 .withConnectionConfiguration(MqttIO.ConnectionConfiguration.create(
                         "tcp://139.162.46.222:1883",
-                        "sample_topic")));
+                        "sample_topic")))
+            .apply(Window.into(
+                    FixedWindows.of(Duration.standardMinutes(1L))))
+            .apply(new CountWords())
+            .apply(MapElements.via(new FormatAsTextFn()))
+            .apply(MqttIO.write().withConnectionConfiguration(MqttIO.ConnectionConfiguration.create(
+                    "tcp://139.162.46.222:1883",
+                    "output_topic")));
         // Extract and sum username/score pairs from the event data.
         //.apply("ExtractUserScore", new ExtractAndSumScore(KeyField.DEVICEID))
         // Write the user and score to the "user_score" BigQuery table.
